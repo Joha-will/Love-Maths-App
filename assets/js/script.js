bef2a73ad3e38ffbd0be7777c0bbd3146ded0419
Joha-will <johawilsonatu@icloud.com>
2022-06-27 13:23:36 +0000

    Added code to calculate two random numbers for the game qestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 
+/**
+ * Thee main game "koop", called when the script is first loaded
+ * and after the user's answer had been processed
+ */
+
 function runGame() {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
 }
+
+runGame();
 
 function checkAnswer() {
 
