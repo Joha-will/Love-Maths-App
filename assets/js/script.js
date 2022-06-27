b87fad390f16b56ec8a566a5a4a14c0d65a719e2
Joha-will <johawilsonatu@icloud.com>
2022-06-27 13:46:34 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute('data-type');
-                    alert(`You click ${gameType}`);
+                runGame(gameType);
 
             }
         })
     }
+
+    runGame("addition");
 })
 
 
 /**
- * Thee main game "koop", called when the script is first loaded
+ * Thee main game "loop", called when the script is first loaded
  * and after the user's answer had been processed
  */
 
@@ -28,9 +30,14 @@ function runGame() {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-}
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw`Unknown game type: ${gameType}. Aborting!`;
+    }
 
-runGame();
+}
 
 function checkAnswer() {
 
@@ -48,7 +55,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1 , operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
